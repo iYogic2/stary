d56f2230765ab010909c277d5c2d0656922fd5f8
iyogic2 <1678130538@qq.com>
2022-08-21 00:15:38 +0800

    feat: 完成prettier配置

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// https://prettier.io/docs/en/configuration.html
+// 会自动包含.editorconfig的内容
+module.exports = {
+  printWidth: 120, //最大横向宽度
+  tabWidth: 2, //缩进
+  useTabs: false, //缩进风格`space`和.editorconfig中的对齐
+  semi: true, //分号结尾
+  singleQuote: true, //单引号替代双引号
+  trailingComma: 'none', //在对象或数组最后一个元素后面是否加逗号
+  bracketSpacing: true //对象间的前后空格
+};
