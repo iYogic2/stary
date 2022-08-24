fbf4839cbbae6d3fd1d0435086b1635ea56dfcdc
iyogic2 <1678130538@qq.com>
2022-08-24 23:03:43 +0800

    feat: 完善commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,48 +1,15 @@
 // https://commitlint.js.org/#/reference-configuration
 // cli：`commitlint --config commitlint.config.js`
+// @commitlint/config-conventional: https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional
+
 module.exports = {
-  /*
-   * Resolve and load @commitlint/config-conventional from node_modules.
-   * Referenced packages must be installed
-   */
-  extends: ['@commitlint/config-conventional']
-  /*
-   * Resolve and load conventional-changelog-atom from node_modules.
-   * Referenced packages must be installed
-   */
-  // parserPreset: 'conventional-changelog-atom',
-  /*
-   * Resolve and load @commitlint/format from node_modules.
-   * Referenced package must be installed
-   */
-  // formatter: '@commitlint/format',
-  /*
-   * Any rules defined here will override rules from @commitlint/config-conventional
-   */
-  // rules: {
-  //   'type-enum': [2, 'always', ['foo']]
-  // }
-  /*
-   * Functions that return true if commitlint should ignore the given message.
-   */
-  // ignores: [(commit) => commit === ''],
-  /*
-   * Whether commitlint uses the default ignore rules.
-   */
-  // defaultIgnores: true
-  /*
-   * Custom URL to show upon failure
-   */
-  // helpUrl: 'https://github.com/conventional-changelog/commitlint/#what-is-commitlint'
-  /*
-   * Custom prompt configs
-   */
-  // prompt: {
-  //   messages: {},
-  //   questions: {
-  //     type: {
-  //       description: 'please input type:'
-  //     }
-  //   }
-  // }
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+    ],
+    'subject-case': [0] //首字母强制小写关闭
+  }
 };
